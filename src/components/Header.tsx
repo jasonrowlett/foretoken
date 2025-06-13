ea7ebb8557632e9f26a27d9a5c230173b42e314a
jasonrowlett <rowlettjason@gmail.com>
2025-06-13 15:14:22 -0500

    Added temporary icon image to upper right of pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,14 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 bg-black">
       <Link to="/">
-        <div className="w-12 h-12">
+        <div className="w-12 h-12 border border-red-500">
           <img
-            src="/assets/foretoken_logo.png"
-            alt="Foretoken Logo"
+            src="https://via.placeholder.com/48x48.png?text=FT"
+            alt="Test Logo"
             className="object-contain w-full h-full"
           />
         </div>
       </Link>
-      {/* Add additional nav items here if needed */}
     </header>
   );
 };
