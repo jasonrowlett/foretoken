687a11cba525df80da9b22623a565fd95c94b94c
jasonrowlett <rowlettjason@gmail.com>
2025-06-13 15:25:01 -0500

    Replaced the websafe logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,15 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 bg-black">
-      <Link to="/" className="text-white text-xl font-bold">
-        Foretoken
+      <Link to="/">
+        <div className="w-12 h-12 border border-red-500">
+          <img
+            src="/assets/foretoken_logo_websafe.png"
+            alt="Foretoken Logo"
+            className="object-contain w-full h-full"
+          />
+        </div>
       </Link>
-      {/* Add additional nav items here if needed */}
     </header>
   );
 };
