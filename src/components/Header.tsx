6c125cfb5186dfcb2f8a81ae7c59876e34b29041
jasonrowlett <rowlettjason@gmail.com>
2025-06-13 14:48:42 -0500

    Upper left logo icon fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,13 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 bg-black">
       <Link to="/">
-        <img
-          src="/assets/foretoken_logo.png"
-          alt="Foretoken Logo"
-          className="h-10 w-10"
-        />
-
+        <div className="w-12 h-12">
+          <img
+            src="/assets/foretoken_logo.png"
+            alt="Foretoken Logo"
+            className="object-contain w-full h-full"
+          />
+        </div>
       </Link>
       {/* Add additional nav items here if needed */}
     </header>
