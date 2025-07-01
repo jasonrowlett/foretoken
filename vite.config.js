ce286cd57afd167d2073cbdaf3dbd317cc5c0342
jasonrowlett <rowlettjason@gmail.com>
2025-07-01 10:37:15 -0500

    Final clean setup for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/foretoken/',
   plugins: [react()],
-  base: '/foretoken/', // 👈 necessary for GitHub Pages under a subpath
   build: {
-    outDir: 'docs', // 👈 GitHub Pages will publish from /docs
+    outDir: 'docs',
   },
 });
