d0420cf8553528e167c2a2b11528f4a284082788
jasonrowlett <rowlettjason@gmail.com>
2025-07-03 15:00:33 -0500

    Switch to static HTML, CSS, and JS boilerplate for GitHub Pages

---
+++ docs/assets/script.js
@@ -0,0 +1,8 @@
+// Vanilla JS version of React render
+document.getElementById("root").innerHTML = `
+  <div class="logo">
+    <img src="./assets/neon-logo.png" alt="Foretoken Logo" />
+  </div>
+  <h1>Welcome to Foretoken</h1>
+  <p>Comparing tokenized and traditional markets, live.</p>
+`;
