796526384a980a331a752f097a498dcc0fac672c
jasonrowlett <rowlettjason@gmail.com>
2025-07-04 09:27:36 -0500

    Corrected html syntax on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,16 +2,16 @@ import HeroSection from "@/components/hero-section";
 import NewsletterSignup from "@/components/newsletter-signup";
 import LiveMarketData from "@/components/live-market-data";
 
-<div id="header"></div>
-
 export default function Home() {
   return (
-    <div className="bg-black">
-      <HeroSection />
-      <NewsletterSignup />
-      <LiveMarketData />
-    </div>
+    <>
+      <div id="header"></div>
+      <div className="bg-black">
+        <HeroSection />
+        <NewsletterSignup />
+        <LiveMarketData />
+      </div>
+      <div id="footer"></div>
+    </>
   );
 }
-
-<div id="footer" />
