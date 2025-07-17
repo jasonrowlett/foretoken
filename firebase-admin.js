16bb20f93d414c93a3ae1169d46a7fa18c9916c9
jasonrowlett <rowlettjason@gmail.com>
2025-07-17 10:17:21 -0500

    Refactor: load Firebase service account from Render secret file

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,7 +1,9 @@
-// firebase-admin.js
 const admin = require('firebase-admin');
 const fs = require('fs');
-const serviceAccount = JSON.parse(fs.readFileSync('foretoken-4e948-firebase-adminsdk-fbsvc-e99027cd03.json', 'utf8'));
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync('/etc/secrets/firebase-service-account.json', 'utf8')
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
